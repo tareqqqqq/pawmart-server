d7ad89e6bee9f30f79a4c4ba37035f30683c1e3b
unknown <tareqmahmudkhan187@gmail.com>
2025-11-08 23:47:50 +0600

    add env file to hide password and create get api for alldata

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const cors = require("cors");
+const { MongoClient, ServerApiVersion} = require('mongodb');
+require('dotenv').config();
+
+
+
+const app = express()
+const port = 3000
+
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+// const uri = "mongodb+srv://pawmart_db:QpSeO2f9dQ52aZYc@cluster0.e80xsto.mongodb.net/?appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e80xsto.mongodb.net/?appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+ const db = client.db("pawmart_db");
+    const productCollection = db.collection("listing");
+    const orders=db.collection("orders")
+
+     app.get("/listing",async(req,res)=>{
+        const result= await productCollection.find().toArray()
+         
+        res.send(result)
+    })
+     app.get("/orders",async(req,res)=>{
+        const result= await orders.find().toArray()
+        res.send(result)
+    })
+
+    app.get("/current-listing", async(req,res)=>{
+        const result= await productCollection.find().sort({date: -1 }).limit(6).toArray()
+        res.send(result)
+    })
+  
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
