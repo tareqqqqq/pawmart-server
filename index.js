2c569a3b318fd0b0f036010ac53c22265d471e8f
unknown <tareqmahmudkhan187@gmail.com>
2025-11-11 17:55:07 +0600

    add search api

--- index.js
+++ index.js
@@ -131,6 +131,14 @@ app.delete("/listing/:id", async (req, res) => {
   const result = await productCollection.deleteOne(query);
   res.send(result);
 });
+
+
+// search 
+app.get("/search", async (req, res) => {
+  const search_text = req.query.search;
+  const result = await productCollection.find({ name: { $regex: search_text, $options: "i" } }).toArray();
+  res.send(result);
+});
   
 
 
